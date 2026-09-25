523d649fddf04ba39805d0f213217c916a05da61
lavigupta88 <lavigupta8814@gmail.com>
2026-09-25 21:33:35 +0530

    Add Task

---
+++ script.js
@@ -0,0 +1,43 @@
+ const todo = document.querySelector("#todo");
+ const progress = document.querySelector("#progress");
+ const done = document.querySelector("#done");
+ let dragElement = null;
+
+ console.log(todo, progress, done);
+
+ const tasks = document.querySelectorAll(".task");
+
+ tasks.forEach(task =>{
+    task.addEventListener("drag", (e) =>{
+        console.log("dragging", e);
+        dragElement = task;
+    })
+ })
+
+function addDragEventsOnColumn(column){
+    column.addEventListener("dragenter", (e)=>{
+        e.preventDefault();
+        column.classList.add("hover-over");
+    })
+    column.addEventListener("dragleave", (e)=>{
+        e.preventDefault();
+        column.classList.remove("hover-over");
+    })
+
+    column.addEventListener("dragover", (e)=>{
+        e.preventDefault();
+    })
+
+    column.addEventListener("drop",(e) =>{
+        e.preventDefault();
+
+        console.log("dropped", dragElement, column);
+
+        column.appendChild(dragElement);
+        colums.classList.remove("hover-over");
+    })
+}
+
+addDragEventsOnColumn(todo);
+addDragEventsOnColumn(progress);
+addDragEventsOnColumn(done);
